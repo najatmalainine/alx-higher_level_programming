075ce15f8903a4e39a3cfe990fdfbff094a86c82
Najat <craazylolla@gmail.com>
2023-09-26 14:08:37 -0700

    m

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Square Module
+"""
+
+
+class Square:
+    """
+    class Square definition
+
+    Args:
+        size (int): size of a side in square
+    """
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
