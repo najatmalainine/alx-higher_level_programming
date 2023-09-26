b2b7251feed8b5e864097f4cb36a65c34462e544
Najat <craazylolla@gmail.com>
2023-09-26 13:54:25 -0700

    m

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -12,8 +12,8 @@ class Square:
         size (int): size of a side in square
     """
     def __init__(self, size=0):
-        self.__size = size
         if size < 0:
             raise ValueError("size must be >= 0")
         elif type(size) != int:
             raise TypeError("size must be an integer")
+        self.__size = size
