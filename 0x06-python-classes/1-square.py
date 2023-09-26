096e9ca4bde06d23fa754763b62abab98a8ced0f
Najat <craazylolla@gmail.com>
2023-09-26 13:41:57 -0700

    m

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module 1-square
+Defines class Square with private attribute size
+"""
+
+
+class Square:
+    """Empty class"""
+    def __init__(self, size):
+        self.__size = size
