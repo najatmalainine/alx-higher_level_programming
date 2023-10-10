f58e1dc94d3ea84d3b76cccb94be0633eafa0153
Najat <craazylolla@gmail.com>
2023-10-10 14:55:25 -0700

    m

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Class Student"""
+
+
+class Student:
+    """Class representing student"""
+    def __init__(self, first_name, last_name, age):
+        """Constructor"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance
+        If attrs is a list of strings, only attribute names
+        contained in this list must be retrieved.
+        Otherwise, all attributes must be retrieved
+        """
+        if attrs is None:
+            return self.__dict__
+        if isinstance(attrs, list):
+            return {attr: getattr(self, attr)
+                    for attr in attrs if hasattr(self, attr)}
