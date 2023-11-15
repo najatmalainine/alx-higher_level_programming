f3bb8ff0a798b11f71e6455931df8d26b55f57c3
Najat <craazylolla@gmail.com>
2023-11-15 12:52:57 -0800

    m

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,8 @@
+-- creates the table unique_id
+-- id INT with the default value 1 and must be unique
+-- name VARCHAR(256)
+
+CREATE TABLE IF NOT EXISTS unique_id (
+	id INT UNIQUE DEFAULT 1,
+	name VARCHAR(256)
+);
