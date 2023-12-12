828b6ef6025e49dee00764a934fcb0b977021ecc
Najat <craazylolla@gmail.com>
2023-12-12 13:47:34 -0800

    m

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-  let count = 0;
-  for (const i in list) {
-    if (list[i] === searchElement) {
-      count++;
-    }
-  }
-  return count;
+  return list.reduce((count, element) => (element === searchElement ? count + 1 : count), 0);
 };
