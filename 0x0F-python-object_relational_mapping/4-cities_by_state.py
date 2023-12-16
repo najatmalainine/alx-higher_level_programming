51e9258f935074cf2c5802cefbf152dced7e0218
Najat <craazylolla@gmail.com>
2023-12-16 13:46:13 -0800

    m

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -27,7 +27,6 @@ if __name__ == "__main__":
     result = cursor.fetchall()
 
     for row in result:
-        if row[1] == argv[4]:
-            print(row)
+        print(row)
     cursor.close()
     conn.close()
