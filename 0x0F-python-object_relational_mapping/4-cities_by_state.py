11431a44c281daa78a5ad38f87d1a5895a79ce95
Najat <craazylolla@gmail.com>
2023-12-16 13:40:49 -0800

    m

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
 
     query = """SELECT cities.id, cities.name, states.name
     FROM states
-    INNER JOIN cities ON states.id = cities.states_id 
+    INNER JOIN cities ON states.id = cities.states_id
     ORDER BY cities.id ASC"""
 
     cursor.execute(query)
