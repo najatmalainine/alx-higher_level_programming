3b9f209563e4749c25393e6b2ad098d7e169c5df
Najat <craazylolla@gmail.com>
2024-01-01 18:32:14 +0000

    m

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -10,13 +10,14 @@
 --Results must be sorted in ascending order by the genre name
 --You can use a maximum of two SELECT statement
 
-SELECT tv_genres.name
-FROM tv_genres
-WHERE tv_genres.name NOT IN (SELECT tv_genres.name
-FROM tv_genres INNER JOIN tv_show_genres
-	ON tv_genres.id = tv_show_genres.genre_id
-	INNER JOIN tv_shows
-	ON tv_shows.id = tv_show_genres.show_id
-	WHERE tv_shows.title = 'Dexter')
-GROUP BY tv_genres.name
-ORDER BY tv_genres.name
+    SELECT tv_genres.name
+      FROM tv_genres
+     WHERE tv_genres.name NOT IN (
+    SELECT tv_genres.name
+      FROM tv_genres
+INNER JOIN tv_show_genres
+        ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+	ON tv_show_genres.show_id = tv_shows.id
+     WHERE tv_shows.title = "Dexter")
+  ORDER BY tv_genres.name ASC;
