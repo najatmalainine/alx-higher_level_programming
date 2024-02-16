46e5779ac916dd806a86095ad57c2a1d55f9d040
Najat <craazylolla@gmail.com>
2024-02-16 23:28:56 +0000

    m

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -6,7 +6,7 @@
 --The database name will be passed as an argument of the mysql command
 
 
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS 'rating'
 FROM tv_shows INNER JOIN tv_show_ratings
 ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.title
